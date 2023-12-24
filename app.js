d14616b6deb1ddd30af1e46364cb56c6dced7edf
SaiHariVamsi <108571655+SaiHariVamsi@users.noreply.github.com>
2023-12-24 15:15:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = Vue.createApp({
 
         updateTotal() {
             this.total = this.total;
-            this.calculate();
+            //this.calculate();
         },
 
         goalChange(){
@@ -30,26 +30,32 @@ const app = Vue.createApp({
             const goalPercentage = this.goal;
             const totalClasses = this.total;
 
-            var currentPercentage = (attendedClasses / totalClasses) * 100;
-
-            this.skipped = skippedClasses;
-
-            var classesNeeded = 0;
-            var classesCanSkip = 0;
-
-            if (currentPercentage < goalPercentage) {
-                var remainingPercentage = goalPercentage - currentPercentage;
-                classesNeeded = Math.ceil((remainingPercentage * totalClasses) / (100 - goalPercentage));
-                this.message = `You need to attend ${classesNeeded} more classes to reach your goal.`;
-            } 
-            else {
-                var excessPercentage = currentPercentage - goalPercentage;
-                classesCanSkip = Math.floor((excessPercentage * totalClasses) / goalPercentage);
-                this.message = `You can skip up to ${classesCanSkip} classes and still meet your goal.`;
+            if (attendedClasses > totalClasses) {
+                this.message = 'baane extralu dengutunnav ga';
             }
+            else{
+                var currentPercentage = (attendedClasses / totalClasses) * 100;
 
-            this.total = totalClasses;
-            this.current = currentPercentage.toFixed(2);
+                this.skipped = skippedClasses;
+
+                var classesNeeded = 0;
+                var classesCanSkip = 0;
+
+                if (currentPercentage < goalPercentage) {
+                    var remainingPercentage = goalPercentage - currentPercentage;
+                    classesNeeded = Math.ceil((remainingPercentage * totalClasses) / (100 - goalPercentage));
+                    this.message = `You need to attend ${classesNeeded} more classes to reach your goal.`;
+                } 
+                else {
+                    var excessPercentage = currentPercentage - goalPercentage;
+                    classesCanSkip = Math.floor((excessPercentage * totalClasses) / goalPercentage);
+                    this.message = `You can skip up to ${classesCanSkip} classes and still meet your goal.`;
+                }
+
+                this.total = totalClasses;
+                this.current = currentPercentage.toFixed(2);
+            }
+            
         }
     }
 });
